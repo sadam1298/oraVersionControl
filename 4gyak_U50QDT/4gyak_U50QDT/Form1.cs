86bf3e8bf9d5c4f77abf9332567f33a4464ee910
Ádám Straub <adam.straub@stud.uni-corvinus.hu>
2021-10-08 13:46:03 +0200

    excel fajl letrehozasa

--- 4gyak_U50QDT/4gyak_U50QDT/Form1.cs
+++ 4gyak_U50QDT/4gyak_U50QDT/Form1.cs
@@ -14,6 +14,9 @@ namespace _4gyak_U50QDT
 {
     public partial class Form1 : Form
     {
+        Excel.Application xlApp;
+        Excel.Workbook xlWB; 
+        Excel.Worksheet xlSheet;
         List<Flat> flats;
         RealEstateEntities context = new RealEstateEntities();
         void LoadData()
@@ -24,6 +27,32 @@ namespace _4gyak_U50QDT
         {
             InitializeComponent();
             LoadData();
+        }
+        void CreateExcel() 
+        {
+            try
+            {                
+                xlApp = new Excel.Application();
+
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                xlSheet = xlWB.ActiveSheet;
+
+                CreateTable();
+               
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) 
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+               
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
     }
 }
