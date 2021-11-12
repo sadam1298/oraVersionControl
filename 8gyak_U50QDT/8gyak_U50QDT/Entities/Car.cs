a88fcaf4075a526a4b61014ee741f355ee6a26ee
Ádám Straub <adam.straub@stud.uni-corvinus.hu>
2021-11-12 12:26:34 +0100

    Auto létrehozása

---
+++ 8gyak_U50QDT/8gyak_U50QDT/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using _8gyak_U50QDT.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8gyak_U50QDT.Entities
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
