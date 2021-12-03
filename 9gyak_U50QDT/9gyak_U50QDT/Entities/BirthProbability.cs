6331cbd3095398fb3f2d20afe14791f2ce773c45
Ádám Straub <adam.straub@stud.uni-corvinus.hu>
2021-12-03 13:12:23 +0100

    Person gender probability classes

---
+++ 9gyak_U50QDT/9gyak_U50QDT/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9gyak_U50QDT.Entities
+{
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double BirthP { get; set; }
+    }
+}
