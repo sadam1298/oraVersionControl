727b6591ff2c05942a779d0159eca036d2489127
Ádám Straub <adam.straub@stud.uni-corvinus.hu>
2021-12-10 15:30:18 +0100

    induló populáció

--- 10gyak_U50QDT/10gyak_U50QDT/Form1.cs
+++ 10gyak_U50QDT/10gyak_U50QDT/Form1.cs
@@ -15,13 +15,20 @@ namespace _10gyak_U50QDT
     {
         GameController gc = new GameController();
         GameArea ga;
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
         public Form1()
         {
             InitializeComponent();
 
             ga = gc.ActivateDisplay();
             Controls.Add(ga);
-            gc.AddPlayer();
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
             gc.Start(true);
         }
     }
